0a4c0f237f808368300f2f284c7f8f62bf477816
Zirconwings <ugonnakpulonu@gmail.com>
2025-11-03 13:31:02 -0700

    Added UI for navigation bars

--- FairDraw/app/src/main/java/com/example/fairdraw/CreateEventPage.java
+++ FairDraw/app/src/main/java/com/example/fairdraw/CreateEventPage.java
@@ -20,5 +20,6 @@ public class CreateEventPage extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
 }
