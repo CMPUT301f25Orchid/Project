3b0d17decfca4813334eea7e33d9220451a18c65
cshayo <shayocleopatra1@gmail.com>
2025-11-06 22:15:54 -0700

    Merged main into branch

--- FairDraw/app/src/main/java/com/example/fairdraw/Activities/ProfileActivity.java
+++ FairDraw/app/src/main/java/com/example/fairdraw/Activities/ProfileActivity.java
@@ -15,6 +15,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.fairdraw.DBs.UserDB;
+import com.example.fairdraw.EditProfileActivity;
 import com.example.fairdraw.Models.User;
 import com.example.fairdraw.R;
 
@@ -24,17 +25,22 @@ public class ProfileActivity extends AppCompatActivity {
 
     //Declaring UI elements
     private Button editButton, returnButton, deleteAccButton, viewHistoryButton;
-    private TextView nameTextView, usernameTextView, emailTextView, phoneTextView;
+    private TextView nameTextView, emailTextView, phoneTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_profile);
-        
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
         //Initializing UI elements
         nameTextView = findViewById(R.id.etName);
-        usernameTextView = findViewById(R.id.etUsername);
         emailTextView = findViewById(R.id.etEmail);
         phoneTextView = findViewById(R.id.etPhone);
 
@@ -48,8 +54,8 @@ public class ProfileActivity extends AppCompatActivity {
 
         if (deviceId != null && !deviceId.isEmpty()){
             //Fetching user data from Firestore based on the device ID
-            // fetchAndDisplayUserData(deviceId);
-            Log.d(TAG, "Device ID received: " + deviceId);
+            fetchAndDisplayUserData(deviceId);
+            //Log.d(TAG, "Device ID received: " + deviceId);
 
         } else {
             //Handle the case where no device ID is provided
@@ -57,6 +63,14 @@ public class ProfileActivity extends AppCompatActivity {
             Toast.makeText(this, "Could not load user profile.", Toast.LENGTH_SHORT).show();
             finish();
         }
+        
+        editButton.setOnClickListener(View -> {
+            // Handle edit button click
+            // Start EditProfileActivity
+            Intent intent = new Intent(ProfileActivity.this, EditProfileActivity.class);
+            intent.putExtra("deviceId", deviceId);
+            startActivity(intent);
+        });
 
         returnButton.setOnClickListener(v -> {
             Intent intent = new Intent(this, EntrantHomeActivity.class);
@@ -78,11 +92,19 @@ public class ProfileActivity extends AppCompatActivity {
                     Toast.makeText(ProfileActivity.this, "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
                     return;
                 }
-                ;
-
                 if (user != null) {
                     //User was found, now update the UI
-                    //Ensure you are on the main
+                    //Ensure you are on the main thread when updating UI
+                    runOnUiThread(() -> {
+                        nameTextView.setText(user.getName());
+                        emailTextView.setText(user.getEmail());
+                        phoneTextView.setText(user.getPhoneNum());
+
+                    });
+                } else {
+                    //User not found
+                    Log.e(TAG, "User not found for device ID: " + deviceId);
+                    Toast.makeText(ProfileActivity.this, "User not found.", Toast.LENGTH_LONG).show();
                 }
 
             }
