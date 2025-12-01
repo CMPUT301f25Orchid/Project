a7ce48ab04faf1012680238ed505029db699cad5
Jeremy Ajoku <jeremyajoku11@gmail.com>
2025-11-30 22:07:09 -0700

    pictures xml

---
+++ FairDraw/app/src/main/java/com/example/fairdraw/Activities/AdminPicturesPage.java
@@ -0,0 +1,26 @@
+package com.example.fairdraw.Activities;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.fairdraw.R;
+
+public class AdminPicturesPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_admin_pictures_page);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
